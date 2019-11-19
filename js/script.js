564b209dd021e5fe788010a2b8a3a022ad679b57
gatchang <move.on.fight@gmail.com>
2019-11-20 01:00:57 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 (function () {
     "use strict";
+    let posData = [];
+    const generatePosData = () => {
+        let $containerList = document.querySelectorAll('.js-container');        
+        $containerList = Array.from($containerList);
+        $containerList.forEach((elm,index)=>{
+
+            if (!posData[index]) {
+				posData[index] = {};
+            }
+            
+            const containerTop = $containerList[index].offsetTop;
+            posData[index].containerTop = containerTop;
+        });
+        console.log(posData);
+        return posData;
+    };
+
+    generatePosData();
+
     const $container = document.querySelector('.js-container');
+
     window.addEventListener('scroll', ()=>{
+
         if (window.pageYOffset > 300) {
             $container.classList.add('is-active');
         } else {
