beb11c5c458622695e46affefcb6e01731aac39b
gatchang <move.on.fight@gmail.com>
2020-01-06 23:02:28 +0900

    Update flickendless.js

--- js/flickendless.js
+++ js/flickendless.js
@@ -1,22 +1,34 @@
 $(function () {
     "use strict";
+    const $quickpick = $('.js-quickpick');
+    const $cover = $('.js-cover');
+    const $selectNum = $('[name=selectNum]');
+    let arryNum = [];
     $('#verticaly1-1').flickEndless({
-        vertical:true
+        vertical:true,
+        onPageChange: function() {
+            arryNum.splice(0,1,this.page);
+            $selectNum.val(arryNum);
+        }
     });
 
     $('#verticaly1-2').flickEndless({
-        vertical:true
+        vertical:true,
+        onPageChange: function() {
+            arryNum.splice(1,1,this.page);
+            $selectNum.val(arryNum);
+        }
     });
 
     $('#verticaly1-3').flickEndless({
-        vertical:true
+        vertical:true,
+        onPageChange: function() {
+            arryNum.splice(2,1,this.page);
+            $selectNum.val(arryNum);
+        }
     });
-
-    const $quickpick = $('.js-quickpick');
-    const $cover = $('.js-cover');
 
     $quickpick.on('change', function(){        
         $(this).closest('div').prev('div').children($cover).toggleClass('is-quickpick');
     });
-    
 });
